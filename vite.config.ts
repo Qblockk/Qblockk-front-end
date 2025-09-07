9d1217a1b4dfb92c7732816da11351fee66c39b2
wkatir <110741874+wkatir@users.noreply.github.com>
2025-09-07 17:52:34 -0600

    feat: add sidebar components

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,4 +11,9 @@ export default defineConfig({
     target: 'react',
     autoCodeSplitting: true,
   }),react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
